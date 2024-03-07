4d385e8a9617b251bf90487d3edf3876e412aefc
axai <02b3akshay@gmail.com>
2024-03-07 15:10:37 +0530

    Add combobox from headless ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import CustomFilter from '@/components/CustomFilter';
 import Hero from '@/components/Hero';
+import SearchBar from '@/components/SearchBar';
 
 export default function Home() {
 	return (
 		<main className="overflow-hidden">
 			<Hero />
+			<div className="mt-12 sm:px-16 px-6 py-4 max-w-screen-xl mx-auto">
+				<div className="flex flex-col items-start justify-start gap-y-2.5 text-black">
+					<h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+					<p>Explore the cars you might like</p>
+				</div>
+				<div className="mt-12 w-full flex-between flex-wrap gap-5">
+					<SearchBar />
+					<div className="flex justify-start flex-wrap items-center gap-2">
+						<CustomFilter title="fuel" />
+						<CustomFilter title="year" />
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState } from 'react';
+import SearchManufacturer from './SearchManufacturer';
+
+export default function SearchBar() {
+	const [manufacturer, setManufacturer] = useState('');
+	function handleSubmit() {}
+
+	return (
+		<form
+			className="flex items-center justify-start max-sm:flex-col w-full relative max-sm:gap-4 max-w-3xl"
+			onSubmit={handleSubmit}>
+			<div className="flex-1 max-sm:w-full flex justify-start items-center relative">
+				<SearchManufacturer
+					manufacturer={manufacturer}
+					setManufacturer={setManufacturer}
+				/>
+			</div>
+		</form>
+	);
+}
